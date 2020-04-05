4381262400a9365c04b860c0de739d0480b60c59
devynnmiller2020 <63205534+devynnmiller2020@users.noreply.github.com>
2020-04-05 12:46:10 -0600

    Create Create_tables_FoodDisplay.sql

---
+++ Create_tables_FoodDisplay.sql
@@ -0,0 +1,13 @@
+create table public.food_display(
+Food_Code bigint
+	,Display_Name varchar(99)
+	,Grains decimal
+	,Vegetables decimal
+	,Fruits decimal
+	,Milk decimal
+	,Meats decimal
+	,Solid_Fats decimal
+	,Added_Sugars decimal
+	,Calories decimal
+	,Saturated_Fats decimal
+);
